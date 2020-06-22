7ce3a83c038c4750a8800a9085ad7e6702984021
Luiz Fernando <luizfernandocosta@protonmail.com>
2020-06-21 21:54:17 -0300

    Versão final do código

--- Código/main.asm
+++ Código/main.asm
@@ -11,11 +11,33 @@ INICIO:
     OUT SPL, R21
 
     RCALL INICIALIZACAO ;Chama a rotina de inicialização
-
     RCALL MAIN ;Começo do programa principal
-
-;--------------------------------------------------------
-
+;*******************************************************************************
+LOOP_PRIMARIO:
+    LDI R21, 1 ;Carrega 1 byte no Registrador 21
+    STS 256, R21 ;Guarda o valor de 256 no espaço de dados
+    RCALL DESLOCABIT ;Chamada relativa a subrotina DESLOCABIT
+    LDI R29, 244 ;Carrega o valor de 244 no Registrador 29
+    LDI R30, 1 ;Carrega o valor de 1 no Registrador de 30
+    RCALL DELAY ;Chamada relativa a subrotina DELAY
+    LDI R21, 0 ;Carrega imediatamente o valor de 0 no Registrador 21
+    STS 257, R21 ;Guarda o valor de R21 na posição 257
+LOOP:
+    LDS R0, 257 ;Carrega o valor de 257 direto no R0
+    INC R0 ;Incrementa R0
+    STS 257, R0 ;Guarda o valor de R0 na posição 257
+    LDI R21, 0 ;Carrega o valor imediato de 0 em Registrador 21
+    STS 256, R21 ;Guarda o valor de R21 na posição 256
+    RCALL DESLOCABIT ;Chamada relativa a subrotina DESLOCABIT
+    LDI R29, 244 ;Carrega o valor imediato de 244 em Registrador 29
+    LDI R30, 1 ;Carrega o valor de  no Registrador 30
+    RCALL DELAY ;Chamada relativa a subrotina DELAY
+    LDS R22, 257 ;Carrega o valor da posição 257 no Registrador 22
+    CPI R22, 7 ;Compara imediatamente o valor do Registrador 22 com o valor de 7, se for menor do que 7, ele continua o loop, se não, ele continua o programa
+    BRLO LOOP ;Branch de condicional relativa, por eu usar o comando CPI antest, a branch vai ocorrer apenas se o valor no registrador for menor que o número representado, caso o valor seja maior, ele continua o programa
+FIMLOOP:
+    RJMP LOOP_PRIMARIO ;Chama o LOOP_PRIMARIO
+;*******************************************************************************
 DELAY: INC R30
 DELAY_START: LDI R26, 20
 DELAY_EXTERNO: LDI R25, 20
@@ -29,24 +51,21 @@ DELAY_INTERNO:
     DEC R30
     BRNE DELAY_START
     RET
-
-    ;----------------------------------------------------
-
+;*******************************************************************************
 MAIN:
-    SBI DDRB, 5 ;Seta bit 5 no DDRB
-    SBI DDRB, 4
-    SBI DDRB, 3
-    SBI DDRB, 2
-    SBI DDRB, 1
-    CBI PORTB, 5
-    CBI PORTB, 4
-    CBI PORTB, 3
-    CBI PORTB, 2
-    CBI PORTB, 1
-    SBI PORTB, 5
-    RET
-
-;------------------------------------------------------
+    SBI DDRB, 5 ;Seta bit na posição 5 no DDRB
+    SBI DDRB, 4 ;Seta bit na posição 4 no DDRB
+    SBI DDRB, 3 ;Seta bit na posição 3 no DDRB
+    SBI DDRB, 2 ;Seta bit na posição 2 no DDRB
+    SBI DDRB, 1 ;Seta bit na posição 1 no DDRB
+    CBI PORTB, 5 ;Limpa bit na posição 5 do PORTB
+    CBI PORTB, 4 ;Limpa bit na posição 4 do PORTB
+    CBI PORTB, 3 ;Limpa bit na posição 3 do PORTB
+    CBI PORTB, 2 ;Limpa bit na posição 2 do PORTB
+    CBI PORTB, 1 ;Limpa bit na posição 1 do PORTB
+    SBI PORTB, 5 ;Setta bit na posição 5 do PORTB
+    RET ;Retorna da subrotina
+;*******************************************************************************
 INICIALIZACAO:
     LDI R21, 0 ;Carrega o valor de 0 no Registrador 21
     OUT PORTB, R21 ;Guarda o registrador na posição do I/O, no caso a PORTB
@@ -54,15 +73,15 @@ INICIALIZACAO:
     OUT PORTC, R21 ;Guarda o registrador na posição do I/O, no caso a PORTC
     LDI R21, 0 ;Carrega o valor de 0 no Registrador 21
     OUT PORTD, R21 ;Guarda o registrador na posição do I/O, no caso a PORTD
-    RET
-;------------------------------------------------------
+    RET ;Retorna da subrotina
+;*******************************************************************************
 DESLOCABIT:
     CBI PORTB, 3 ;Limpa o bit 3 no PORTB
     CBI PORTB, 4 ;Limpa o bit 4 no PORTB
     CBI PORTB, 1 ;Limpa o bit 1 no PORTB
     LDS R5, 256 ;Carrega o valor da posição 256 no Registrador 5
-    SBRC R5, 0
-    SBI PORTB, 1
-    SBI PORTB, 4
-    SBI PORTB, 3
-    RET
+    SBRC R5, 0 ;Compara se o R5 é igual a 0, se for igual, ele não executa a próxima linha, se for diferente, ele executta a próxima linha
+    SBI PORTB, 1 ;Seta bit na posição 1 no PORTB
+    SBI PORTB, 4 ;Seta bit na posição 4 no PORTB
+    SBI PORTB, 3 ;Seta bit na posição 3 no PORTB
+    RET ;Retorna da subrotina
